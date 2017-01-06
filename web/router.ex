dfd8a9d9fd82578a9ff4c837dae542461ed9d9b0
Miriam Moser <mirjoy.moser@gmail.com>
2017-01-06 11:30:02 -0500

    added different scopes for public and private routes

--- web/router.ex
+++ web/router.ex
@@ -5,12 +5,16 @@ defmodule MiriamBlogApi.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :json_public do
+    plug :accepts, ["json-api"]
+  end
+
   pipeline :json_api do
     plug :accepts, ["json-api"]
 
     plug JaSerializer.ContentTypeNegotiation
     plug JaSerializer.Deserializer
-    
+
     plug Guardian.Plug.VerifyHeader, realm: "Bearer"
     plug Guardian.Plug.LoadResource
     plug Guardian.Plug.EnsureAuthenticated, handler: TokenController
@@ -28,10 +32,18 @@ defmodule MiriamBlogApi.Router do
     get "/logged_in_page", LoggedInController, :logged_in_page
   end
 
+  # public routes
+  scope "/v1", as: :v1, alias: MiriamBlogApi do
+    pipe_through :json_public
+
+    resources "/blogs", BlogController, only: [:index, :show]
+  end
+
+  # private routes
   scope "/v1", as: :v1, alias: MiriamBlogApi do
     pipe_through :json_api
 
-    resources "/blogs", BlogController
+    resources "/blogs", BlogController, only: [:new, :edit, :create, :update, :delete]
   end
 
 end
